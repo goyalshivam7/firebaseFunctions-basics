a2f49aa43a23fedde1ad9d6733f01a1be1a852f8
Shivam Goyal <60148611+shivamgoyal7@users.noreply.github.com>
2020-07-18 12:05:42 +0530

    shivam/cloud functions

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
     apiKey: "AIzaSyBO_VmAX-_0hGPDTV4pyX6Vl1TWkJ7wwe8",
-    authDomain: "functions-ed3d3.firebaseapp.com",
-    databaseURL: "https://functions-ed3d3.firebaseio.com",
-    projectId: "functions-ed3d3",
-    storageBucket: "functions-ed3d3.appspot.com",
-    messagingSenderId: "789185524716",
-    appId: "1:789185524716:web:c90d51d9c252acc66386c9",
+    authDomain: ...,
+    databaseURL:....,
+    projectId: .....,
+    storageBucket: .....,
+    messagingSenderId: .....,
+    appId: ......,
     measurementId: "G-GMYWERG86H"
   };
   // Initialize Firebase
